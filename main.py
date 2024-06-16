f211fe680998633d630882b441101eb92d588d17
karakimashita223 <nikitayarostburi123@gmail.com>
2024-06-16 21:16:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from telebot import TeleBot
+from flask import Flask, request
+from telebot import TeleBot, types
 import os
 import random
 import schedule
@@ -7,10 +8,13 @@ import threading
 from datetime import datetime, timedelta
 
 
-TOKEN = '7243199722:AAE0Fl7E0k0XEXjPqwK_hu3YqMSapMKq1IU'
+TOKEN = os.getenv('7243199722:AAE0Fl7E0k0XEXjPqwK_hu3YqMSapMKq1IU')
+WEBHOOK_URL = os.getenv('https://asian-bot.onrender.com')
+PORT = int(os.getenv('PORT', 5000))
 BOT_USERNAME = 'asian_everyday_bot'
 
 bot = TeleBot(TOKEN)
+app = Flask(__name__)
 
 IMAGES_FOLDER = "images"
 TEXT_FILE = "phrases.txt"
@@ -114,4 +118,18 @@ def handle_photo(message):
 
 load_phrases()
 
-bot.polling(non_stop=True, interval=0)
+@app.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_str = request.get_data().decode('UTF-8')
+    update = types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '!', 200
+
+@app.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL + TOKEN)
+    return '!', 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=PORT)
